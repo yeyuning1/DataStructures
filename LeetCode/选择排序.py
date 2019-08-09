8b66956cd92e4ed2064ca125cf936b7246845007
yeyuning1 <yeyuning1@gmail.com>
2019-08-09 23:43:40 +0800

    更新排序算法

---
+++ LeetCode/选择排序.py
@@ -0,0 +1,10 @@
+def select_sort(origin_items, comp=lambda x, y: x < y):
+    """简单选择排序"""
+    items = origin_items[:]
+    for i in range(len(items) - 1):
+        min_index = i
+        for j in range(i + 1, len(items)):
+            if comp(items[j], items[min_index]):
+                min_index = j
+        items[i], items[min_index] = items[min_index], items[i]
+    return items
